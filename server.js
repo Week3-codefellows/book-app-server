e37ecd7a0926005eb5a084e15ef148f99ee8ce83
Ed Abrahamsen <esabrahamsen@yahoo.com>
2017-12-19 13:14:17 -0800

    server update

--- server.js
+++ server.js
@@ -15,5 +15,14 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
+app.get('/api/v1/books', (req, res) => {
+    client.query('SELECT book_id title author image_url FROM books;')
+      .then(results => res.send(results.rows))
+      .catch(err => {
+        console.error(err)
+        res.status(400).send('something went wrong, Bozo')
+      })
+  })
+
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
