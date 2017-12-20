95b9cb87179b3769c1787ee851f1a76f04b95afb
Caitlin <caitlinsweeneylmt@gmail.com>
2017-12-20 07:30:57 -0800

    updating server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 const express = require('express');
 const cores = require('cors');
 const pg = require('pg');
@@ -14,6 +15,11 @@ client.on('error', err => console.error(err));
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
+app.get('/api/v1/books',(req,res)=>{
 
+  client.query('SELECT * FROM books;')
+  .then(results=> res.send(results.rows))
+  .catch(console.error)
+})
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
