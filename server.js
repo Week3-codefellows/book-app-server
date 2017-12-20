165df2df32b348889b8b71caa2b99fd825c0d5b2
Caitlin <caitlinsweeneylmt@gmail.com>
2017-12-20 07:54:48 -0800

    updated server.js

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 'use strict';
 
-
+const app = express();
+const PORT = process.env.PORT;
 const express = require('express');
 const cores = require('cors');
 const pg = require('pg');
 
-const app = express();
-const PORT = process.env.PORT;
 
-const client = new pg.Client(process.env.CLIENT_URL);
+
+
+const client = new pg.Client(DATABASE_URL);
 client.connect();
-client.on('error', err => console.error(err));
+client.on('error', console.error);
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
+
 app.get('/api/v1/books',(req,res)=>{
 
-  client.query('SELECT * FROM books;')
+  client.query('SELECT book_id, title, author, image_url, isbn FROM books;')
   .then(results=> res.send(results.rows))
   .catch(console.error)
 })
-app.get('*', (req, res) => res.redirect(CLIENT_URL));
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+app.all('*',(req,res)=> res.redirect(CLIENT_URL));
+app.listen(PORT, ()=> console.log('Listening on ${PORT}'));
