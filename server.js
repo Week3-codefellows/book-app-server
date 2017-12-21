3dc9a2c402c2a6a677f2f1a01219a06a71ceec25
Ed Abrahamsen <esabrahamsen@yahoo.com>
2017-12-21 09:02:17 -0800

    add post

--- server.js
+++ server.js
@@ -1,28 +1,42 @@
 'use strict'
 
 // Application Dependencies
-const express = require('express')
-const cors = require('cors')
-const pg = require('pg')
+const express = require('express');
+const cors = require('cors');
+const pg = require('pg');
 
 // App Setup
-const app = express()
-const PORT = process.env.PORT
-const DATABASE_URL = process.env.DATABASE_URL
-const CLIENT_URL = process.env.CLIENT_URL
+const app = express();
+const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+const CLIENT_URL = process.env.CLIENT_URL;
 
 // DATABASE Setup
-const client = new pg.Client(DATABASE_URL)
-client.connect()
-client.on('error', console.error)
+const client = new pg.Client(DATABASE_URL);
+client.connect();
+client.on('error', console.error);
 
-app.use(cors())
+app.use(cors());
 
 app.get('/api/v1/books', (req, res) => {
-  client.query('SELECT book_id, title, author, image_url FROM books;')
+  client.query('SELECT book_id, title, author, image_url, isbn FROM books;')
     .then(results => res.send(results.rows))
     .catch(console.error)
-})
+});
 
-app.all('*', (req, res) => res.redirect(CLIENT_URL))
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+app.get('/api/v1/books/:id', (req, res) => {
+  client.query(`SELECT * FROM books WHERE book_id=$1;`, [req.params.id])
+    .then(result => res.send(result.rows))
+    .catch(console.error)
+});
+
+app.post('/api/v1/books', (req, res) => {
+  client.query(`INSERT INTO books(title, author, image_url, isbn, description)
+  VALUES($1, $2, $3, $4 $5);`,
+    [req.body.title, req.body.author, req.body.image_url, req.body.isbn, req.body.description])
+    .then(result => res.send(result))
+    .catch(console.error)
+});
+
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
